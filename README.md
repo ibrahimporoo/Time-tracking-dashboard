8d9dcf914b2073ca2cb2318dcc0ddc2a0f03adff
Ibrahim Abdo <ibrahimporoo@gmail.com>
2022-09-09 02:01:16 +0200

    Uploading my challenge's Solution

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
+# Frontend Mentor - Time tracking dashboard
+
+![Design preview for the Time tracking dashboard coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
+
+**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+
 # Time-tracking-dashboard
 The challenge is to build out this dashboard and get it looking as close to the design as possible.
---
+++ js/main.js
@@ -0,0 +1,30 @@
+let timesValues = document.querySelectorAll(".times li");
+let allTimeTitles = document.querySelectorAll(".time-title");
+let currentHours = document.querySelectorAll("span.current");
+let prevHours = document.querySelectorAll("span.previous");
+let timeFrame = "weekly";
+updateData(timeFrame);
+
+timesValues.forEach(time => {
+	time.addEventListener("click", (e) => {
+		timesValues.forEach(time => {
+			time.classList.remove("on");
+		})
+		e.currentTarget.classList.add("on");
+		timeFrame = e.target.innerText.toLowerCase();
+		updateData(timeFrame);
+	})
+});
+function updateData(timeFrame) {
+	fetch("../data.json").then(result => {
+		let datajson = result.json();
+		return datajson;
+	}).then(datajson => {
+		for(let i = 0; i < datajson.length; i++) {
+			allTimeTitles[i].innerText = timeFrame;
+			prevHours[i].innerText = datajson[i].timeframes[timeFrame].previous;
+			prevHours[i].innerText = datajson[i].timeframes[timeFrame].previous;
+		}
+	})
+}
+
